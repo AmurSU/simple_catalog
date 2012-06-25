cd754e651e756b9407576d132715d7490ffeebe7
Novikov Andrey <envek@envek.name>
2012-06-25 11:31:30 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleCatalog::Application.config.secret_token = 'fd303e1cd100b2da534f365ee6272c80f4165f6e6f9b43a2006f1a5a0ad9a277b6a217b254f8f26ab49fc0243968a6890d93de1f085135512d095bce346d4dcb'
