363d25345708182edb79cf2473a5d13dc475aa92
Novikov Andrey <envek@envek.name>
2012-06-29 11:28:06 +1000

    A bit of devise tuning

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,14 @@ class ApplicationController < ActionController::Base
     config.actions.exclude :show
   end
 
+private
+
+  def after_sign_in_path_for(resource_or_scope)
+    addresses_path
+  end
+
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
+
 end
