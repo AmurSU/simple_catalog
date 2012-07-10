f854454e131920bad2dd427ed0a4577238d3054b
Novikov Andrey <envek@envek.name>
2012-07-10 10:18:08 +1000

    A bit of fixes and enhancements

--- app/assets/javascripts/guest_catalog.js
+++ app/assets/javascripts/guest_catalog.js
@@ -65,7 +65,7 @@ jQuery(document).ready(function($) {
     var sector_id = parseInt($(this).attr('data-sector-id'));
     var discipline_id = parseInt($(this).attr('data-discipline-id'));
     container.empty();
-    $('<p><a class="catalog_show_addresses" data-sector-id="'+sector_id+'" data-discipline-id="'+discipline_id+'" href="/catalog/'+sector_id+'">Показать интернет-адреса</a></p>').appendTo(container);
+    $('<p><a class="catalog_show_addresses" data-sector-id="'+sector_id+'" data-discipline-id="'+discipline_id+'" href="/catalog/'+sector_id+'/'+discipline_id+'">Показать интернет-адреса</a></p>').appendTo(container);
     return false;
   });
 
